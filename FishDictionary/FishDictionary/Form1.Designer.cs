4bbc783b4c33474675711329947250efbe15ea5e
Mark Sandifer <45290434+Mark-Sandifer@users.noreply.github.com>
2022-10-10 19:10:42 +0100

    Fish Dictionary
    When building the application please insert the files from the Resources folder into the same folder as the .exe

---
+++ FishDictionary/FishDictionary/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace FishDictionary
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.labelFishDescrip = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // treeView1
+            // 
+            this.treeView1.HideSelection = false;
+            this.treeView1.Location = new System.Drawing.Point(12, 12);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(191, 616);
+            this.treeView1.TabIndex = 0;
+            this.treeView1.NodeMouseClick += new System.Windows.Forms.TreeNodeMouseClickEventHandler(this.treeView1_NodeMouseClick);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Controls.Add(this.labelFishDescrip);
+            this.panel1.Location = new System.Drawing.Point(209, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(807, 616);
+            this.panel1.TabIndex = 1;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(6, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(796, 306);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // labelFishDescrip
+            // 
+            this.labelFishDescrip.AutoSize = true;
+            this.labelFishDescrip.Location = new System.Drawing.Point(3, 312);
+            this.labelFishDescrip.Name = "labelFishDescrip";
+            this.labelFishDescrip.Size = new System.Drawing.Size(97, 13);
+            this.labelFishDescrip.TabIndex = 0;
+            this.labelFishDescrip.Text = "                              ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1028, 640);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.treeView1);
+            this.Name = "Form1";
+            this.Text = "FishDictionary";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label labelFishDescrip;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ FishDictionary/FishDictionary/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics.PerformanceData;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace FishDictionary
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public Dictionary<string, Dictionary<string, FishImageDescrip>> Fish = new Dictionary<string, Dictionary<string, FishImageDescrip>>{};
+
+        public void Form1_Load(object sender, EventArgs e)
+        {
+            string lines = File.ReadAllText("KindsOfFish.txt");
+            string[] data = lines.Split(',');
+
+            for (int i = 0; i < data.Length-1; i += 5)
+            {
+                FishImageDescrip insertInfo = new FishImageDescrip();
+                data[i] = data[i].Replace("\n", "").Replace("\r", "");
+                if (Fish.ContainsKey(data[i]) == false)
+                {
+                    Fish.Add(data[i], new Dictionary<string,FishImageDescrip>());
+                }
+                Fish[data[i]].Add(data[i + 1], insertInfo);
+                insertInfo.Name = data[i + 2];
+                insertInfo.Image = data[i + 3];
+                insertInfo.Description = data[i + 4];
+            }
+            foreach (KeyValuePair<string, Dictionary<string, FishImageDescrip>> waterTypes in Fish)
+            {
+                TreeNode node = new TreeNode();
+                node.Text = waterTypes.Key;
+                foreach (KeyValuePair<string, FishImageDescrip> fish in waterTypes.Value)
+                {
+                    TreeNode node2 = new TreeNode();
+                    node2.Text = fish.Key;
+                    node.Nodes.Add(node2);
+                    TreeNode fishNode = new TreeNode();
+                    fishNode.Text = fish.Value.Name;
+                    fishNode.Tag = fish.Value;
+                    node2.Nodes.Add(fishNode);
+
+                }
+                treeView1.Nodes.Add(node);
+            }
+        }
+        private void treeView1_NodeMouseClick(object sender, TreeNodeMouseClickEventArgs e)
+        {
+            if(e.Node.Nodes.Count == 0)
+            {
+                FishImageDescrip fish = (FishImageDescrip)e.Node.Tag;
+                labelFishDescrip.Text = fish.Description;
+                pictureBox1.ImageLocation = fish.Image;
+
+            }
+        }
+    }
+    public class FishImageDescrip
+    {
+        public string Name;
+        public string Image;
+        public string Description;
+    }
+}
